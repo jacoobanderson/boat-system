5cec0333541bc4cddb4a85f49d32acdb9740dba4
jacoobanderson <j00cwa@gmail.com>
2022-03-15 21:48:12 +0100

    Create Initial UI functionality

--- app/src/main/java/assignment4/App.java
+++ app/src/main/java/assignment4/App.java
@@ -14,18 +14,40 @@ public class App {
    * @param args Unused program arguments.
    */
   public static void main(String[] args) {
-    Member member = new Member("Jacob", "j00cwa@gmail.com", "123456");
+    App app = new App();
+    app.start();
+    // Member member = new Member("Jacob", "j00cwa@gmail.com", "123456");
 
-    Motorboat boatOne = new Motorboat("test", "Motorboat", 20, 200);
-    Motorsailer boatTwo = new Motorsailer("testTwo", "Motorsailer", 30, 250, 2);
-    Canoe boatThree = new Canoe("testThree", "Canoe", 40);
-    Sailboat boatFour = new Sailboat("testThree", "Sailboat", 40, 1);
+    // Motorboat boatOne = new Motorboat("test", "Motorboat", 20, 200);
+    // Motorsailer boatTwo = new Motorsailer("testTwo", "Motorsailer", 30, 250, 2);
+    // Canoe boatThree = new Canoe("testThree", "Canoe", 40);
+    // Sailboat boatFour = new Sailboat("testThree", "Sailboat", 40, 1);
 
-    member.addBoat(boatOne);
-    member.addBoat(boatTwo);
-    member.addBoat(boatThree);
-    member.addBoat(boatFour);
+    // member.addBoat(boatOne);
+    // member.addBoat(boatTwo);
+    // member.addBoat(boatThree);
+    // member.addBoat(boatFour);
 
-    member.showMemberInfo();
+    // member.showMemberInfo();
+  }
+
+  public void start() {
+    ConsoleUi ui = new ConsoleUi();
+    String action = ui.startUi();
+    handleInitialAction(ui, action);
+  }
+
+  public void handleInitialAction(ConsoleUi ui, String action) {
+    switch (action) {
+      case "1":
+        ui.createMemberView();
+        break;
+      case "2":
+        System.out.print("MEMBERS");
+        break;
+      case "3":
+        ui.selectMember();
+        break;
+    }
   }
 }
--- app/src/main/java/assignment4/ConsoleUi.java
+++ app/src/main/java/assignment4/ConsoleUi.java
@@ -1,5 +1,42 @@
 package assignment4;
 
+import java.util.Scanner;
+
 public class ConsoleUi {
-    
+    private Scanner scan = new Scanner(System.in, "UTF-8");
+
+    public String startUi() {
+        System.out.println("Choose what you want to do:");
+        System.out.println("1. Create a new member");
+        System.out.println("2. List all members");
+        System.out.println("3. Select a member");
+
+        String answer = readInput();
+        scan.close();
+        return answer;
+    }
+
+    public String[] createMemberView() {
+        System.out.println("Enter your name:");
+        String name = readInput();
+
+        System.out.println("Enter your email (optional):");
+        String email = readInput();
+
+        String[] member = {name, email};
+        scan.close();
+        return member;
+    }
+
+    public String selectMember() {
+        System.out.println("Enter the name of the member:");
+        String name = readInput();
+        scan.close();
+        return name;
+    }
+
+    public String readInput() {;
+        String input = scan.nextLine();
+        return input;
+    }
 }
