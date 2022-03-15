01b9777a9221e52cb7c3cc14669cc482d6996ccb
jacoobanderson <j00cwa@gmail.com>
2022-03-15 19:22:28 +0100

    Create initial boat parent class

--- app/src/main/java/assignment4/Boat.java
+++ app/src/main/java/assignment4/Boat.java
@@ -1,5 +1,42 @@
 package assignment4;
 
-public class Boat {
-    
+public abstract class Boat {
+    private String name;
+    private String type;
+    private int length;
+
+    public Boat() {
+    }
+
+    public Boat(String name, String type, int length) {
+        setName(name);
+        setType(type);
+        setLength(length);
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setType(String type) {
+        this.type = type;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public void setLength(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {
+        return length;
+    }
+
+    public abstract String showBoatInfo();
 }
